25429510ad0a3dcb8384ca64a81261ebbbba1ace
Erwanleroy <erwanlr56@gmail.com>
2024-06-19 19:32:12 +0200

    ajout de lacces a indexed db pour panel

--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -1,30 +1,50 @@
-// Panel.js
 import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import { getData } from '../Utils/indexeddb';
 
-const Panel = ({clickLien, onUpdatePage}) => {
+const Panel = ({ clickLien, onUpdatePage }) => {
   const theme = useTheme().palette; // Obtenez le thème courant
-  const jsonData = require('../Utils/donnees.json'); // Charger le fichier JSON
-  let componentsList = jsonData.categories.map(category => category.name);
-  // Enlever la catégorie "home" du tableau des noms de catégories
-  componentsList = componentsList.filter(name => name !== "IGP ToolBox");
+  const [data, setData] = React.useState([]);
+  const [componentsList, setComponentsList] = React.useState([]);
+
+  React.useEffect(() => {
+    // Charger les données depuis IndexedDB lors du montage du composant
+    async function loadData() {
+      const data = await getData();
+      if (data) {
+        setData(data);
+      }
+    }
+    loadData();
+  }, []);
+
+  React.useEffect(() => {
+    if (data && data.categories) {
+      // Filtrer et exclure la catégorie "IGP ToolBox"
+      const filteredComponents = data.categories
+        .map(category => category.name)
+        .filter(name => name !== "IGP ToolBox");
+
+      setComponentsList(filteredComponents);
+    }
+  }, [data]);
 
   const clickItem = e => {
-    clickLien()
-    onUpdatePage(e.target.innerText)
-  }
+    clickLien();
+    onUpdatePage(e.target.innerText);
+  };
 
   return (
-    <Box style={{backgroundColor: theme.background.paper, color:theme.primary.main, borderColor:theme.primary.main}} sx={{ width: 250 }} className="pannel" role="presentation" >
+    <Box style={{ backgroundColor: theme.background.paper, color: theme.primary.main, borderColor: theme.primary.main }} sx={{ width: 250 }} className="pannel" role="presentation">
       <List>
-        {componentsList.map((text, index) => (
-          <ListItem key={index} onClick={clickItem} style={{cursor:"pointer"}} className='listItemPerso'>
+        {componentsList && componentsList.map((text, index) => (
+          <ListItem key={index} onClick={clickItem} style={{ cursor: "pointer" }} className='listItemPerso'>
             <ListItemText primary={text} />
-          </ListItem>    
+          </ListItem>
         ))}
       </List>
     </Box>
